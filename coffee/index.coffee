34f9d0fc7449c9e131c115fbeb13466a09a4e394
Aldo Mendez <aldomendez86@gmail.com>
2015-03-19 11:02:19 -0500

    Models and views

--- coffee/index.coffee
+++ coffee/index.coffee
@@ -1,20 +1,43 @@
+# Taken from http://arturadib.com/hello-backbonejs/docs/2.html
 do (jQuery)->
 	$ = jQuery;
+	Item = Backbone.Model.extend {
+		defaults:{
+			part1: 'hello'
+			part2: 'world'
+		}
+	}
+	List = Backbone.Collection.extend {
+		model: Item
+	}
 	ListView = Backbone.View.extend {
-		el: $ '#container'
+		el:$ '#container'
 		events:{
 			'click button#add':'addItem'
 		}
 		initialize:()->
-			_.bindAll this, 'render', 'addItem'
+			_.bindAll this, 'render', 'addItem', 'appendItem'
+			this.collection = new List()
+			this.collection.bind 'add', this.appendItem
+
 			this.counter = 0
-			this.render();
+			this.render()
 		render:()->
+			self = this
 			$(this.el).append "<button id='add'>Add list item</button>"
 			$(this.el).append "<ul></ul>"
-			this.addItem()
+			_(this.collection.models).each (item)->
+				self.appendItem(item)
+			,this
 		addItem:()->
 			this.counter++
-			$('ul', this.el).append " <li>hello world #{this.counter}</li> "
+			item = new Item()
+			item.set {
+				part2: item.get('part2') + this.counter
+			}
+			this.collection.add(item)
+		appendItem:(item)->
+			$('ul', this.el).append "<li>#{item.get('part1')} #{item.get('part2')}</li>"
 	}
+
 	listView = new ListView()
--- js/index.js
+++ js/index.js
@@ -1,24 +1,47 @@
 (function(jQuery) {
-  var $, ListView, listView;
+  var $, Item, List, ListView, listView;
   $ = jQuery;
+  Item = Backbone.Model.extend({
+    defaults: {
+      part1: 'hello',
+      part2: 'world'
+    }
+  });
+  List = Backbone.Collection.extend({
+    model: Item
+  });
   ListView = Backbone.View.extend({
     el: $('#container'),
     events: {
       'click button#add': 'addItem'
     },
     initialize: function() {
-      _.bindAll(this, 'render', 'addItem');
+      _.bindAll(this, 'render', 'addItem', 'appendItem');
+      this.collection = new List();
+      this.collection.bind('add', this.appendItem);
       this.counter = 0;
       return this.render();
     },
     render: function() {
+      var self;
+      self = this;
       $(this.el).append("<button id='add'>Add list item</button>");
       $(this.el).append("<ul></ul>");
-      return this.addItem();
+      return _(this.collection.models).each(function(item) {
+        return self.appendItem(item);
+      }, this);
     },
     addItem: function() {
+      var item;
       this.counter++;
-      return $('ul', this.el).append(" <li>hello world " + this.counter + "</li> ");
+      item = new Item();
+      item.set({
+        part2: item.get('part2') + this.counter
+      });
+      return this.collection.add(item);
+    },
+    appendItem: function(item) {
+      return $('ul', this.el).append("<li>" + (item.get('part1')) + " " + (item.get('part2')) + "</li>");
     }
   });
   return listView = new ListView();

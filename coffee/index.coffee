f8057a744e21e16e4d4b21ad7ab832d694f6ed58
Aldo Mendez <aldomendez86@gmail.com>
2015-03-19 12:52:15 -0500

    Proebas con manejo de memoria

--- coffee/index.coffee
+++ coffee/index.coffee
@@ -1,6 +1,10 @@
 # Taken from http://arturadib.com/hello-backbonejs/docs/2.html
 do (jQuery)->
 	$ = jQuery;
+
+	Backbone.sync = (method, model, success, error)->
+		success()
+
 	Item = Backbone.Model.extend {
 		defaults:{
 			part1: 'hello'
@@ -10,13 +14,42 @@ do (jQuery)->
 	List = Backbone.Collection.extend {
 		model: Item
 	}
-	ListView = Backbone.View.extend {
-		el:$ '#container'
-		events:{
-			'click button#add':'addItem'
+
+	ItemView = Backbone.View.extend {
+		tagName:'li'
+		events: {
+			'click span.swap': 'swap'
+			'click span.delete': 'remove'
 		}
 		initialize:()->
-			_.bindAll this, 'render', 'addItem', 'appendItem'
+			_.bindAll this, 'render', 'unrender', 'swap', 'remove'
+
+			this.model.bind 'change', this.render
+			this.model.bind 'remove', this.unrender
+		render:()->
+			$(this.el).html "<span>#{this.model.get 'part1'} #{this.model.get 'part2'}</span> <span class='swap'>[swap]</span><span class='delete'>[remove]</span>"
+			return this # for chainable calls, like `.render().el`
+		unrender:()->
+			$(this.el).remove()
+		swap:()->
+			swapped = {
+				part1: this.model.get 'part2'
+				part2: this.model.get 'part1'
+			}
+			this.model.set swapped
+		remove:()->
+			this.model.destroy()
+	}
+
+	ListView = Backbone.View.extend {
+		el: $ '#container'
+		events:{
+			'click button#add':'addItem'
+			'click button#multiAdd':'addLotsOfItems'
+			'click button#deleteAll':'deleteAll'
+		}
+		initialize:()->
+			_.bindAll this, 'render', 'addItem', 'appendItem','addLotsOfItems', 'deleteAll'
 			this.collection = new List()
 			this.collection.bind 'add', this.appendItem
 
@@ -24,20 +57,33 @@ do (jQuery)->
 			this.render()
 		render:()->
 			self = this
-			$(this.el).append "<button id='add'>Add list item</button>"
+			$(this.el).append "<button id='add'>Add list item</button> <button id='multiAdd'>Add 1000</button> <button id='deleteAll'>Delete All</button>"
 			$(this.el).append "<ul></ul>"
-			_(this.collection.models).each (item)->
+			_(this.collection.models).each (item)-> # in case collection is not empty
 				self.appendItem(item)
 			,this
+		addLotsOfItems:()->
+			self = this
+			_([0..1000]).each (item)->
+				self.addItem()
+			,this
+		deleteAll:()->
+			# console.log this.collection
+			this.collection.reset()
+			this.$el.empty()
+			this.render()
 		addItem:()->
 			this.counter++
 			item = new Item()
 			item.set {
-				part2: item.get('part2') + this.counter
+				part2: item.get('part2') + " " + this.counter
 			}
 			this.collection.add(item)
 		appendItem:(item)->
-			$('ul', this.el).append "<li>#{item.get('part1')} #{item.get('part2')}</li>"
+			itemView = new ItemView {
+				model: item
+			}
+			$('ul', this.el).append itemView.render().el
 	}
 
 	listView = new ListView()
--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 (function(jQuery) {
-  var $, Item, List, ListView, listView;
+  var $, Item, ItemView, List, ListView, listView;
   $ = jQuery;
+  Backbone.sync = function(method, model, success, error) {
+    return success();
+  };
   Item = Backbone.Model.extend({
     defaults: {
       part1: 'hello',
@@ -10,13 +13,45 @@
   List = Backbone.Collection.extend({
     model: Item
   });
+  ItemView = Backbone.View.extend({
+    tagName: 'li',
+    events: {
+      'click span.swap': 'swap',
+      'click span.delete': 'remove'
+    },
+    initialize: function() {
+      _.bindAll(this, 'render', 'unrender', 'swap', 'remove');
+      this.model.bind('change', this.render);
+      return this.model.bind('remove', this.unrender);
+    },
+    render: function() {
+      $(this.el).html("<span>" + (this.model.get('part1')) + " " + (this.model.get('part2')) + "</span> <span class='swap'>[swap]</span><span class='delete'>[remove]</span>");
+      return this;
+    },
+    unrender: function() {
+      return $(this.el).remove();
+    },
+    swap: function() {
+      var swapped;
+      swapped = {
+        part1: this.model.get('part2'),
+        part2: this.model.get('part1')
+      };
+      return this.model.set(swapped);
+    },
+    remove: function() {
+      return this.model.destroy();
+    }
+  });
   ListView = Backbone.View.extend({
     el: $('#container'),
     events: {
-      'click button#add': 'addItem'
+      'click button#add': 'addItem',
+      'click button#multiAdd': 'addLotsOfItems',
+      'click button#deleteAll': 'deleteAll'
     },
     initialize: function() {
-      _.bindAll(this, 'render', 'addItem', 'appendItem');
+      _.bindAll(this, 'render', 'addItem', 'appendItem', 'addLotsOfItems', 'deleteAll');
       this.collection = new List();
       this.collection.bind('add', this.appendItem);
       this.counter = 0;
@@ -25,23 +60,43 @@
     render: function() {
       var self;
       self = this;
-      $(this.el).append("<button id='add'>Add list item</button>");
+      $(this.el).append("<button id='add'>Add list item</button> <button id='multiAdd'>Add 1000</button> <button id='deleteAll'>Delete All</button>");
       $(this.el).append("<ul></ul>");
       return _(this.collection.models).each(function(item) {
         return self.appendItem(item);
       }, this);
+    },
+    addLotsOfItems: function() {
+      var self, _i, _results;
+      self = this;
+      return _((function() {
+        _results = [];
+        for (_i = 0; _i <= 1000; _i++){ _results.push(_i); }
+        return _results;
+      }).apply(this)).each(function(item) {
+        return self.addItem();
+      }, this);
+    },
+    deleteAll: function() {
+      this.collection.reset();
+      this.$el.empty();
+      return this.render();
     },
     addItem: function() {
       var item;
       this.counter++;
       item = new Item();
       item.set({
-        part2: item.get('part2') + this.counter
+        part2: item.get('part2') + " " + this.counter
       });
       return this.collection.add(item);
     },
     appendItem: function(item) {
-      return $('ul', this.el).append("<li>" + (item.get('part1')) + " " + (item.get('part2')) + "</li>");
+      var itemView;
+      itemView = new ItemView({
+        model: item
+      });
+      return $('ul', this.el).append(itemView.render().el);
     }
   });
   return listView = new ListView();

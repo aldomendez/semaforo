e0ad7aea494b72a1dbbf2d1ca345cb5388339e03
Aldo Mendez <aldomendez86@gmail.com>
2015-04-07 23:05:51 -0500

    mejoras visuales

--- simple.templates/page.php
+++ simple.templates/page.php
@@ -7,64 +7,60 @@
   <link rel="stylesheet" type="text/css"  href="http://cymautocert/osaapp/jsLib/SemanticUi/1.11.6/semantic.css">
 </head>
   <style>
-  .ui.tiny.labels .label, .ui.tiny.label {
-    margin-bottom: 3px;
+  .ui.big.labels .label, .ui.big.label {
+    margin-bottom: 5px;
   }
 </style>
 <body>
-  
-    <!-- El contenido del template ira aqui -->
-    <div class="ui grid" id="container">
-      <div class="row">
-        <div class="column">
-          <div class="ui menu">
-            <a href="http://cymautocert/osaapp/semaforo-dev/simple.php" class="item"><i class="home icon"></i>Avago</a>
-        
-          </div>
+  <div class="ui grid page" id="container">
+    <div class="row">
+      <div class="column">
+        <div class="ui menu">
+          <a href="http://cymautocert/osaapp/semaforo-dev/simple.php" class="item"><i class="home icon"></i>Avago</a>
+      
         </div>
       </div>
-      <div class="row">
-        <div class="column">
-          {% set cardsNum = bu|length  %}
-          <div class="ui {{size[cardsNum]}} cards">
-            {% for manager, areas in bu %}
-            <div class="card">
-              <div class="content">
-                <div class="header"><a href="./simple.php/{{manager}}"><small>Equipos de </small>{{manager}}</a></div>
-                <div class="ui list">
-                  {% for areaName, procesos in areas %}
-                    <div class="item">
-                      <div class="content"><b>{{areaName}}</b></div>
-                      <div class="description">
-                        <ui class="list">
-                          {% for processName, machines in procesos %}
-                          <div class="item">
-                            <div class="content">{{processName}}</div>
-                            <div class="description">
-                              {% for machineName, machine in machines %}
-                                <div class="label horizontal tiny ui {{machine.STATUS}}">
-                                  {{machine.NAME}}
-                                  {% if machine.STATUS == 'red' %}
-                                    <div class="detail"><i class="icon warning sign"></i>{{machine.DIFF}}</div>
-                                  {% endif %}
-                                </div>
-                              {% endfor %}
-                            </div>
+    </div>
+    <div class="row">
+      <div class="column">
+        {% set cardsNum = bu|length  %}
+        <div class="ui {{size[bu|length]}} cards">
+          {% for manager, areas in bu %}
+          <div class="card">
+            <div class="content">
+              <h1 class="header"><a href="simple.php/{{manager}}"><small>Equipos de </small>{{manager}}</a></h1>
+              <div class="ui list">
+                {% for areaName, procesos in areas %}
+                  <div class="item">
+                    <div class="content"><b>{{areaName}}</b></div>
+                    <div class="description">
+                      <ui class="list">
+                        {% for processName, machines in procesos %}
+                        <div class="item">
+                          <div class="content">{{processName}}</div>
+                          <div class="description">
+                            {% for machineName, machine in machines %}
+                              <div class="label horizontal big ui {{machine.STATUS}}">
+                                {{machine.NAME}}
+                                {% if machine.STATUS == 'red' %}
+                                  <div class="detail"><i class="icon warning sign"></i>{{machine.DIFF}}</div>
+                                {% endif %}
+                              </div>
+                            {% endfor %}
                           </div>
-                          {% endfor %}
-                        </ui>
-                      </div>
+                        </div>
+                        {% endfor %}
+                      </ui>
                     </div>
-                  {% endfor %}
-                </div>
+                  </div>
+                {% endfor %}
               </div>
             </div>
-            {% endfor %}
           </div>
+          {% endfor %}
         </div>
       </div>
-
     </div>
-      
+  </div>
 </body>
 </html>
